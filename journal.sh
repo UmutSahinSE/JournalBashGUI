68dfef511252884b86ec8bec605de8a4572d1630
Umut Şahin <umut.sahin.se@gmail.com>
2018-04-18 08:50:48 +0300

    Can write for today

--- journal.sh
+++ journal.sh
@@ -4,16 +4,45 @@ profileName=""
 profilePassword=""
 authenticationFlag=0
 
+function finisher(){
+	rm result.txt #*-*-*.txt 
+	exit 0
+}
+
+function writeForToday(){
+	day=$(date | awk '{print $3;}' )
+	month=$(date | awk '{print $2;}' )
+	year=$(date| awk '{print $6;}' )
+	"" > $day-$month-$year.txt 
+	dialog --clear  --backtitle $appName --title "$day/$month/$year" --editbox $day-$month-$year.txt 80 80   2> "$day-$month-$year.txt"
+	dialog --clear  --backtitle $appName --title "$day/$month/$year" --msgbox "Saved on $day-$month-$year.txt" 20 50
+
+}
+
+function writeForAnotherDay(){
+	echo a
+}
+
+function readADay(){
+	echo a
+}
+
+function editADay(){
+	echo a
+}
+
+
+
 function createProfile(){
 	while true
 	do
-		dialog --clear  --backtitle $appName --title "Create Profile" --inputbox "Your Name" 20 50 2> "result.txt"
+		dialog --clear  --backtitle $appName --title "Create Profile" --gauge "Your Name" 20 50 2> "result.txt"
 		profileName=$(cat result.txt)
 		if ! [[ $profileName =~ ^[a-zA-Z]+$ ]]
 		then
 			dialog --clear  --backtitle $appName --title "Problem"  --yesno "Please only use letters a-z without spaces or special characters, try again?" 20 50 2> "result.txt"
 			if [[ $? == 1  ]]
-			then exit 0 
+			then finisher 
 			fi
 		else
 			break	
@@ -28,7 +57,6 @@ function createProfile(){
 		fi
 	done
 	profilePassword=$(cat result.txt)
-	mkdir entries
 	authenticationFlag=1
 }
 
@@ -48,8 +76,7 @@ function prepareProfile(){
 	do
 		dialog --clear  --backtitle $appName --title "Create Profile" --passwordbox "Your Password" 20 50 2> "result.txt"
 		profilePassword=$(cat result.txt)
-		bg unzip -P $profilePassword $profileName.zip
-	        sleep 1	
+		unzip -uP $profilePassword $profileName.zip	
 		if  [[ -e "aaa.sh" ]] 
 		then 
 			break
@@ -65,17 +92,27 @@ function prepareProfile(){
 
 appName="Journal"
 
-dialog --clear  --backtitle $appName --title "Accessing Profile" --menu "Options" 50 50 3 1 "Already Existing Profile" 2 "New Profile" 3 "Exit" 2> "result.txt"
+dialog --clear  --backtitle $appName --title "Accessing Profile" --menu "Options" 20 50 3 1 "Already Existing Profile" 2 "New Profile" 3 "Exit" 2> "result.txt"
 menuitem=$(cat result.txt)
  
 case $menuitem in
 	1) prepareProfile;;
 	2) createProfile;;
-	3) exit 0;;	
+	3) finisher;;	
 esac
 
 if [[ $authenticationFlag == 1 ]]
-then echo "a"
-fi
-#dialog --clear  --backtitle $appName --title "Main Menu" --menu "Choose one" 20 20 4 1 "Write for Today" 2 "Write for Another Day" 3 "Read a Day" 4 "Edit a Day" 
+then
+	dialog --clear  --backtitle $appName --title "Main Menu" --menu "Choose one" 20 50 5 1 "Write for Today" 2 "Write for Another Day" 3 "Read a Day" 4 "Edit a Day" 5 "Exit"  2> "result.txt"
+	menuitem=$(cat result.txt)
+
+case $menuitem in
+        1) writeForToday;;
+        2) writeForAnotherDay;;
+        3) readADay;;
+	4) editADay;;
+	5) finisher;;
+esac
+	finisher
+fi 
 
